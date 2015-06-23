5b98c0d327bbaeb9b1896e07a3de0ba0e472379f
guoxc <qazwsxedc121@gmail.com>
2015-06-23 15:05:38 +0800

    p6_1001

---
+++ src/p6_1001.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * User: guoxc
+ * Date: 15-6-23
+ * Time: 15:02
+ */
+public class p6_1001 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i = 0; i < n; i += 1){
+            long a = in.nextLong();
+            long b = in.nextLong();
+            long c = in.nextLong();
+            if(a + b > c){
+                System.out.println("Case #" + (i + 1) + ": true");
+            }else{
+                System.out.println("Case #" + (i + 1) + ": false");
+            }
+        }
+    }
+}
